092398d03bc11835c2eb9d86c00f437004d46c8a
Ben Sekyondwa <ben.sekyondwa@external.telekom.de>
2024-06-21 12:25:26 +0200

    Initial Commit

---
+++ WeatherApp/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  WeatherApp
+//
+//  Created by Ben Sekyondwa on 21/06/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Ben Sekyondwa on 21/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
